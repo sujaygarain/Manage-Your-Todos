f7eecb060c0ea337d9f17175c0735ec31ce21f4d
Sujay Garain <sgtech.ex@gmail.com>
2025-01-07 12:51:20 +0530

    first commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,34 @@
 
 
+// // src/firebaseConfig.js
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDhwWuB4Lfaewgqwy1o2-A-cFAB7-BHrDs",
+//   authDomain: "todo-context-ebcb5.firebaseapp.com",
+//   projectId: "todo-context-ebcb5",
+//   storageBucket: "todo-context-ebcb5.appspot.com",
+//   messagingSenderId: "866151393014",
+//   appId: "1:866151393014:web:453fff4af41610c527aed7",
+// };
+
+// // // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// // const firebaseConfig = {
+// //   apiKey: "AIzaSyDhwWuB4Lfaewgqwy1o2-A-cFAB7-BHrDs",
+// //   authDomain: "todo-context-ebcb5.firebaseapp.com",
+// //   projectId: "todo-context-ebcb5",
+// //   storageBucket: "todo-context-ebcb5.firebasestorage.app",
+// //   messagingSenderId: "866151393014",
+// //   appId: "1:866151393014:web:453fff4af41610c527aed7",
+// //   measurementId: "G-NT0XJR6WHY"
+// // };
+// export const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+// export const db = getFirestore(app);
+
+
 // src/firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -9,14 +38,17 @@ const firebaseConfig = {
   apiKey: "AIzaSyDhwWuB4Lfaewgqwy1o2-A-cFAB7-BHrDs",
   authDomain: "todo-context-ebcb5.firebaseapp.com",
   projectId: "todo-context-ebcb5",
-  storageBucket: "todo-context-ebcb5.appspot.com",
+  storageBucket: "todo-context-ebcb5.firebasestorage.app",
   messagingSenderId: "866151393014",
   appId: "1:866151393014:web:453fff4af41610c527aed7",
 };
 
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
 
 // // firebaseConfig.js
 // import { initializeApp } from "firebase/app";
